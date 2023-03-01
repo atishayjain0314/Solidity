cfdee06777e6b5f7ad4ed71ecebd8c00e3907159
200110022atishay <atishayjain0314@gmail.com>
2023-03-01 17:58:35 +0530

    access

---
+++ accesss.sol
@@ -0,0 +1,31 @@
+//SPDX-License-Identifier: MIT
+
+
+
+contract AccessControl {
+    event GrantRole(bytes32 indexed _role,address indexed account);
+    event RevokeRole(bytes32 indexed _role,address indexed account);
+    mapping(bytes32=>mapping(address=>bool)) public roles;
+    bytes32 private constant ADMIN = keccak256(abi.encodePacked("ADMIN"));
+    bytes32 private constant USER = keccak256(abi.encodePacked("USER"));
+    
+    modifier onlyRole(bytes32 _role){
+        require(roles[_role][msg.sender],"not authorized");
+        _;
+    }
+    constructor(){
+        _grantRole(ADMIN,msg.sender);
+    }
+    function _grantRole(bytes32 _role,address _account) internal{
+        roles[_role][_account]=true;
+        emit GrantRole(_role,_account);
+    }
+    function grantrole(bytes32 _role,address _account) external onlyRole(ADMIN){
+        _grantRole(_role,_account);
+    }
+    function revokerole(bytes32 _role,address _account) external onlyRole(ADMIN){
+        roles[_role][_account]=false;
+        emit RevokeRole(_role,_account);
+    }
+    
+}
